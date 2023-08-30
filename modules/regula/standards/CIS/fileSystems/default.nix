45de0801eb568bf1ef81bab7b3a983d05835a591
Jermeiah S <owner@arouzing.xyz>
2023-08-30 15:10:29 -0400

    init config for testing

---
+++ modules/regula/standards/CIS/fileSystems/default.nix
@@ -0,0 +1,34 @@
+{
+config,
+lib,
+pkgs,
+...
+}:
+let
+    inherit (lib) mkIf mkEnableOption mkOption types;
+    cfg = config.regula;
+in {
+    options = {
+        regula = {
+            cis = {
+                filesystems = {
+                    enable = mkEnableOption { default = true; };
+                };
+            };
+        };
+    };
+    config = mkIf cfg.enable {
+        assertions = [
+            #1.1.1.1 Ensure mounting of cramfs filesystems is disabled
+            {
+                asserttion = builtins.elm "cramfs" config.boot.blacklistedKernelModules;
+                message = "boot.blacklistedKernelModules must contain cramfs";
+            }
+            # 1.1.1.2 Ensure mounting of freevxfs filesystems is disabled
+            {
+                asserttion = builtins.elm "freevxf" config.boot.blacklistedKernelModules;
+                message = "boot.blacklistedKernelModules must contain freevxf";
+            }
+        ];
+    };
+}
