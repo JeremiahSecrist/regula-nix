6ef2e4407aa4b5c7c07c333b81e6e6443e422e8e
Jermeiah S <owner@arouzing.xyz>
2023-08-30 23:06:17 -0400

    feat: added more filesystems and mount points

--- modules/regula/standards/cis/fileSystems/default.nix
+++ modules/regula/standards/cis/fileSystems/default.nix
@@ -7,6 +7,37 @@ pkgs,
 let
     inherit (lib) mkIf mkEnableOption mkOption types;
     cfg = config.regula;
+
+    mkBlacklistedFilesystem = modules: map (x: {
+                assertion = builtins.elem "${x}" config.boot.blacklistedKernelModules;
+                message = "CIS: 1.1.1.x boot.blacklistedKernelModules must contain ${x}";
+            }) modules;
+
+    unusedFilesystems = mkBlacklistedFilesystem [
+        "cramfs"
+        "freevxf"
+        "jffs2"
+        "hfs"
+        "hfsplus"
+        "squashfs"
+        "udf"
+        "vfat"
+    ];
+
+    mkMountAsserts = mounts: map (x: {
+                assertion = builtins.hasAttr "${x}" config.fileSystems;
+                message = "CIS 1.1.x ${x} must be mounted seperately";
+            }) mounts;
+
+    mountAsserts = mkMountAsserts [
+        "/tmp"              # 1.1.4
+        "/var"              # 1.1.6
+        "/var/tmp"          # 1.1.7
+        "/var/log"          # 1.1.11
+        "/var/log/audit"    # 1.1.12
+        "/home"             # 1.1.13
+        "/dev/shm"          # 1.1.15
+        ];
 in {
     options = {
         regula = {
@@ -18,17 +49,6 @@ in {
         };
     };
     config = mkIf cfg.enable {
-        assertions = [
-            #1.1.1.1 Ensure mounting of cramfs filesystems is disabled
-            {
-                asserttion = builtins.elm "cramfs" config.boot.blacklistedKernelModules;
-                message = "boot.blacklistedKernelModules must contain cramfs";
-            }
-            # 1.1.1.2 Ensure mounting of freevxfs filesystems is disabled
-            {
-                asserttion = builtins.elm "freevxf" config.boot.blacklistedKernelModules;
-                message = "boot.blacklistedKernelModules must contain freevxf";
-            }
-        ];
+        assertions = unusedFilesystems ++ mountAsserts;
     };
 }
