522a0f2f7224dfa482e95f55d39bf559ab6b0fbf
Jermeiah S <owner@arouzing.xyz>
2023-08-30 23:05:46 -0400

    feat: adjusted test as assets are expected to fail

--- tests/default.nix
+++ tests/default.nix
@@ -1,5 +1,6 @@
 {
 self,
+inputs,
 pkgs,
 ...
 }:
@@ -9,29 +10,22 @@ let
     # Since it's common for CI not to have $DISPLAY available, we have to explicitly tell the tests "please don't expect any screen available"
     virtualisation.graphics = false;
   };
-
+  nixpkgs = inputs.nixpkgs;
 in pkgs.nixosTest {
-  # NixOS tests are run inside a virtual machine, and here we specify system of the machine.
-  system = "x86_64-linux";
+    name = "basic test";
+    nodes.machine = { config, pkgs, ...}: {
+      imports = [
+        self.nixosModules.regula
+      ];
+      boot.loader.systemd-boot.enable = true;
+      boot.loader.efi.canTouchEfiVariables = true;
+      regula.enable = true;
+      system.stateVersion = "23.05";
+    };
 
-  nodes = {
-    server = { config, pkgs, ... }: {
-      imports = [ sharedModule self.nixosModules.default ];
-
-      users = {
-        mutableUsers = false;
-        users = {
-          # For ease of debugging the VM as the `root` user
-          root.password = "";
-        };
-      };
-
-      };
-
-  # Disable linting for simpler debugging of the testScript
-  skipLint = true;
-
-  testScript = ''
-  '';
-  };
-}
+    testScript = {nodes, ...}: ''
+      # machine.wait_for_unit("default.target")
+      # machine.succeed("su -- jane -c 'which firefox'")
+      # machine.fail("su -- root -c 'which firefox'")
+    '';
+  }
