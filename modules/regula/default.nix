e6248cbb11ffbcce2f3d2fb32e7ef475215e371a
Jermeiah S <owner@arouzing.xyz>
2024-08-10 13:53:15 -0400

    formatting

--- modules/regula/default.nix
+++ modules/regula/default.nix
@@ -1,4 +1,4 @@
-{ config, lib, pkgs, ... }:
+{ config, lib, pkgs, modulesPath, ... }:
 let
   inherit (lib) mkIf mkEnableOption mkOption types mapAttrsToList;
   cfg = config.regula;
@@ -7,8 +7,16 @@ in
 {
   options = import ./options.nix { inherit lib; };
   config = mkIf cfg.enable {
-    warnings = rlib.failedAssertionsToListOfStr (rlib.regulaToAssertion config.regula.rules "warning");
-    assertions = (rlib.regulaToAssertion config.regula.rules "assertion");
+    warnings =
+      (rlib.failedAssertionsToListOfStr
+        (rlib.regulaToAssertion config.regula.rules "warning"));
+    assertions = [
+      {
+        assertion = (0 == (lib.length (builtins.filter (x: (x.mode == "warning" && x.buildValidation != null)) (lib.mapAttrsToList (n: v: v) cfg.rules))));
+        message = "can't use build validator with warning";
+      }
+    ] ++ (rlib.regulaToAssertion config.regula.rules "assertion");
     system.checks = map (x: (rlib.checkFile x.buildValidation)) (builtins.filter (x: (x.mode == "buildValidation" && x.enable)) (mapAttrsToList (n: v: v) cfg.rules));
   };
 }
+
