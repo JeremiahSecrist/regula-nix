f873dcac8a80fa84fa50b79074393ad11406c8dc
Jermeiah S <owner@arouzing.xyz>
2024-08-10 13:52:25 -0400

    modify lib for meta usage

--- modules/regula/lib.nix
+++ modules/regula/lib.nix
@@ -1,4 +1,5 @@
-{ lib, pkgs, }: {
+{ lib, pkgs, }: rec {
+  mapListOfAttrsStringOfnl = inp: (lib.concatStringsSep "\n" (lib.flatten (builtins.map (x: (lib.mapAttrsFlatten (n: v: "${n}: ${v}") x)) inp)));
   failedAssertionsToListOfStr = listOfAttrs:
     map (x: x.message) (builtins.filter (x: !x.assertion) listOfAttrs);
   regulaToAssertion = x: mode:
@@ -7,12 +8,20 @@
         (if v.verboseMessage != "" then
           builtins.traceVerbose v.verboseMessage v
         else
-          v)) x));
-  checkFile = { runCommandLocal ? pkgs.runCommandLocal, name
-    , nativeBuildInputs ? [ ], script ? "", file ? "" }:
-    runCommandLocal name {
-      inherit nativeBuildInputs file;
-      passAsFile = [ "file" ];
-    } # sh
-    script;
+          { inherit (v) mode enable assertion; message = (mapListOfAttrsStringOfnl v.meta.discovery); }))
+      x));
+  checkFile =
+    { runCommandLocal ? pkgs.runCommandLocal
+    , name
+    , nativeBuildInputs ? [ ]
+    , script ? ""
+    , file ? ""
+    }:
+    runCommandLocal name
+      {
+        inherit nativeBuildInputs file;
+        passAsFile = [ "file" ];
+      } # sh
+      script;
+  flakeModulePath = builtins.toString ../.;
 }
