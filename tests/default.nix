3d293f1e42bd26b271ca1c221e92d006dd087da5
Jermeiah S <owner@arouzing.xyz>
2024-08-12 11:12:41 -0400

    rewrite test area

--- tests/default.nix
+++ tests/default.nix
@@ -4,16 +4,19 @@ let
   sharedModule = {
     # Since it's common for CI not to have $DISPLAY available, we have to explicitly tell the tests "please don't expect any screen available"
     virtualisation.graphics = false;
-  };
-in pkgs.nixosTest {
-  name = "basic test";
-  nodes.machine = { config, pkgs, ... }: {
     imports = [ self.nixosModules.regula ];
     boot.loader.systemd-boot.enable = true;
     boot.loader.efi.canTouchEfiVariables = true;
     regula.enable = true;
     # regula.profiles.cis.server-1.enable = true;
     system.stateVersion = "23.05";
+
+  };
+in
+pkgs.testers.runNixOSTest {
+  name = "basic test";
+  nodes.machine = { config, pkgs, ... }: {
+    imports = [ sharedModule ];
   };
 
   testScript = { nodes, ... }: "";
