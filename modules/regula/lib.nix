03216907124eff801b3285b289feceac8a016967
Jermeiah S <owner@arouzing.xyz>
2024-08-12 19:51:38 -0400

    cleaned up code for lib

--- modules/regula/lib.nix
+++ modules/regula/lib.nix
@@ -1,14 +1,14 @@
 { lib, pkgs, }:
 let
-  __div = (x: y: y x);
+  __div = (x: y: y x); # Will remove when pipes (|>) are in mainline
 in
 rec {
   showFailedAssertion = x: builtins.filter (y: !y.assertion) x;
-  mapListOfAttrsStringOfnl = inp:  (inp / (builtins.map (x: (lib.mapAttrsFlatten (n: v: "${n}: ${v}") x) )) / lib.flatten) / (x: lib.concatStringsSep "\n" x);
-  failedAssertionsToListOfStr = listOfAttrs: listOfAttrs / showFailedAssertion / (i: map (x: x.message) i);
+  mapListOfAttrsStringOfnl = inp: inp / builtins.map (lib.mapAttrsFlatten (n: v: "${n}: ${v}")) / lib.flatten / (x: lib.concatStringsSep "\n" x);
+  failedAssertionsToListOfStr = listOfAttrs: listOfAttrs / showFailedAssertion / (map (x: x.message));
   regulaToAssertion = x: mode: x /
-  lib.mapAttrsToList (n: v: { inherit (v) mode enable assertion; message = mapListOfAttrsStringOfnl v.meta.discovery;})
-  / builtins.filter (x: x.enable && x.mode == mode);
+    lib.mapAttrsToList (n: v: { inherit (v) mode enable assertion; message = mapListOfAttrsStringOfnl v.meta.discovery; })
+    / builtins.filter (x: x.enable && x.mode == mode);
   checkFile =
     { runCommandLocal ? pkgs.runCommandLocal
     , name
