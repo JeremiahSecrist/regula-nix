09693a44bdbb3eaaeda1b324908eaff8eeec5269
Jermeiah S <owner@arouzing.xyz>
2024-08-12 11:03:38 -0400

    rewrite lib to used cursed div, will remove when |> exists

--- modules/regula/lib.nix
+++ modules/regula/lib.nix
@@ -1,15 +1,14 @@
-{ lib, pkgs, }: rec {
-  mapListOfAttrsStringOfnl = inp: (lib.concatStringsSep "\n" (lib.flatten (builtins.map (x: (lib.mapAttrsFlatten (n: v: "${n}: ${v}") x)) inp)));
-  failedAssertionsToListOfStr = listOfAttrs:
-    map (x: x.message) (builtins.filter (x: !x.assertion) listOfAttrs);
-  regulaToAssertion = x: mode:
-    (builtins.filter (x: (x.enable && x.mode == mode)) (lib.mapAttrsToList
-      (n: v:
-        (if v.verboseMessage != "" then
-          builtins.traceVerbose v.verboseMessage v
-        else
-          { inherit (v) mode enable assertion; message = (mapListOfAttrsStringOfnl v.meta.discovery); }))
-      x));
+{ lib, pkgs, }:
+let
+  __div = (x: y: y x);
+in
+rec {
+  showFailedAssertion = x: builtins.filter (y: !y.assertion) x;
+  mapListOfAttrsStringOfnl = inp:  (inp / (builtins.map (x: (lib.mapAttrsFlatten (n: v: "${n}: ${v}") x) )) / lib.flatten) / (x: lib.concatStringsSep "\n" x);
+  failedAssertionsToListOfStr = listOfAttrs: listOfAttrs / showFailedAssertion / (i: map (x: x.message) i);
+  regulaToAssertion = x: mode: x /
+  lib.mapAttrsToList (n: v: { inherit (v) mode enable assertion; message = mapListOfAttrsStringOfnl v.meta.discovery;})
+  / builtins.filter (x: x.enable && x.mode == mode);
   checkFile =
     { runCommandLocal ? pkgs.runCommandLocal
     , name
