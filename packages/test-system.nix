accb866ed0e970a26378eae4bcceefca212a53bc
Jermeiah S <owner@arouzing.xyz>
2024-08-27 19:39:04 -0400

    cleaned up test environment

--- packages/test-system.nix
+++ packages/test-system.nix
@@ -19,16 +19,18 @@
     enable = true;
     features = {
       nixosTesting.enable = true;
-      # assertions.enable = true;
+      assertions.enable = true;
       packageChecks.enable = true;
     };
-    # assertions.enable = true;
-    # buildValidation.vmTest.enable = true;
     rules = {
       mo = {
-        enable = true;
         tests = {
-          eval.assertion.is = false;
+          eval.assertion.is = config.services.openssh.enable;
+          vm.testScript = ''
+            with subtest("""@discovery"""):
+              machine.wait_for_unit("sshd.service")
+              machine.succeed("systemctl is-active -q sshd.service")
+          '';
           build.packageCheck.package =
             {
               discovery,
@@ -51,62 +53,8 @@
           name = "mo";
         };
       };
-      emo = {
-        enable = true;
-        tests = {
-          # enable = true;
-          vm.testScript = ''
-            with subtest("""@discovery"""):
-              machine.wait_for_unit("sshd.service")
-              machine.succeed("systemctl is-active -q sshd.service")
-          '';
-        };
-        meta.discovery = {
-          path = builtins.toString ./test-system.nix;
-          A = "HI";
-        };
-      };
-      demo = {
-        enable = true;
-        tests = {
-          # enable = true;
-          vm.testScript = ''
-            with subtest("""@discovery"""):
-              machine.wait_for_unit("sshd.service")
-              machine.succeed("systemctl is-active -q sshd.service")
-          '';
-        };
-        meta.discovery = {
-          path = builtins.toString ./test-system.nix;
-          A = "HI";
-        };
-      };
-    };
-    # sshEnableForceRuntime = {
-    #   enable = true;
-    #   mode = "nixosTest";
-    #   vm.testScript = ''
-    #     with subtest("sshd must be enabled"):
-    #       machine.wait_for_unit("sshd.service")
-    #       machine.succeed("systemctl is-active -q sshd.service")
-    #   '';
-    # };
-    # sshEnableForce = {
-    #   enable = true;
-    #   mode = "assertion";
-    #   assertion = config.services.openssh.enable;
-    #   meta = {
-    #     discovery = [ { message = "openssh must be enabled!"; } ];
-    #   };
-    # };
-    # };
-  };
 
-  assertions = [
-    # {
-    #   message = config.regula.rules.demo.tests.vm._testScriptCompiled;
-    #   assertion = false;
-    # }
-  ];
+    };
+  };
   system.stateVersion = "${config.system.nixos.release}";
 }
